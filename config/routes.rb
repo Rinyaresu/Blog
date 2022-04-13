4f7343b5c80edd79a5269642b85a8fb87e5620e7
rinyaresu <kaiquelinhares17@outlook.com>
2022-04-13 07:53:37 -0300

    feat: specifying routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :articles
+  resources :articles, except: [:index]
   devise_for :users
-  resources :about
+  resources :about, only: [:index]
   get 'main/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
