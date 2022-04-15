e2982034b26c9699810a415344fc27010bbf7113
rinyaresu <79602114+Rinyaresu@users.noreply.github.com>
2022-04-15 09:53:16 -0300

    feat: make articles update in real time

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,6 @@ class Article < ApplicationRecord
   has_rich_text :content
   validates :title, presence: true, length: { maximum: 255 }
   validates :content, presence: true
+
+  after_update_commit { broadcast_update }
 end
