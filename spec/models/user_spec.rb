3530c9e6705bb2645761046df0bace989657c9a6
rinyaresu <kaiquelinhares17@outlook.com>
2022-04-17 21:09:32 -0300

    test: add rspec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+RSpec.describe User, type: :model do
+  # check if the model is valid
+  it "is valid with valid attributes" do
+    user = User.new(username: "John", email: "user@user.com", password: "password")
+    expect(user).to be_valid
+  end
+end
