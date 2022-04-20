9367ad07026d12a8da257de4076f4fa5561029c2
rinyaresu <kaiquelinhares17@outlook.com>
2022-04-20 13:53:11 -0300

    refactor: removing sign_up again

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_scope :user do
     get "/sign_in" => "devise/sessions#new" # custom path to login/sign_in
-    get "/sign_up" => "devise/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
   end
 
   resources :articles, except: [:index]
