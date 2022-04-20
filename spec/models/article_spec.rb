3b7181be8e5ae2a28f443f6015cb93691170b557
rinyaresu <kaiquelinhares17@outlook.com>
2022-04-20 13:53:42 -0300

    test: article model

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -6,6 +6,16 @@ RSpec.describe Article, type: :model do
   # create a test to check that the article is not saved without a title
   it "is not valid without a title" do
     article = Article.new
+    article.title = ""
+    article.content = "test"
+    expect(article).not_to be_valid
+  end
+
+  # create a test to check that the article is not saved without a body
+  it "is not valid without a content" do
+    article = Article.new
+    article.title = "test"
+    article.content = ""
     expect(article).not_to be_valid
   end
 end
