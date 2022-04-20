2feb92dc248939c18226177b7ce22e4b1d604674
rinyaresu <kaiquelinhares17@outlook.com>
2022-04-20 13:53:52 -0300

    test: user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,4 +8,23 @@ RSpec.describe User, type: :model do
     user = User.new(username: "John", email: "user@user.com", password: "password")
     expect(user).to be_valid
   end
+
+  # check if the model is invalid
+  it "is invalid without a username" do
+    user = User.new(username: nil)
+    user.valid?
+    expect(user.errors[:username]).to include("can't be blank")
+  end
+
+  it "is invalid without an email" do
+    user = User.new(email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank")
+  end
+
+  it "is invalid without a password" do
+    user = User.new(password: nil)
+    user.valid?
+    expect(user.errors[:password]).to include("can't be blank")
+  end
 end
