f96ca717bbad4c1bb2c28ba553db93e9e40a3378
rinyaresu <kaiquelinhares17@outlook.com>
2022-04-21 13:43:49 -0300

    test: articles model test

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,40 @@
 require "test_helper"
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should have a title and content" do
+      article = Article.new
+      assert_not article.valid?
+      assert_equal ["can't be blank"], article.errors[:title]
+      assert_equal ["can't be blank"], article.errors[:content]
+    end
+
+  test "should have a title and max length of 255" do
+    article = Article.new(title: "a" * 256)
+    assert_not article.valid?
+    assert_equal ["is too long (maximum is 255 characters)"], article.errors[:title]
+  end
+
+  # shold broadcast update after update commit
+  test "should broadcast update after update commit" do
+    article = articles(:one)
+    assert_difference(
+      ActionCable.server.broadcast("articles",
+                                                    { action: "update", id: article.id,
+                                                      title: article.title, content: article.content
+                                                    })) do
+      article.update(title: "new title", content: "new content")
+    end
+  end
+
+  # should slug title
+  test "should slug title" do
+    article = articles(:one)
+    assert_equal "MyString", article.slug
+  end
+
+  # should have rich text content
+  test "should have rich text content" do
+    article = articles(:one)
+    assert_equal "In a million stars!", article.content.to_plain_text
+  end
 end
