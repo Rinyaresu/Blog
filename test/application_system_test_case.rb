6b2eddef9dcb55c54ac910fb913a691c7f4f352a
Rinyaresu <kaiquelinhares17@outlook.com>
2022-05-13 15:23:15 -0300

    add chrome

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,5 +3,5 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome
+  driven_by :selenium, using: :chrome, screen_size: [425, 1426]
 end
